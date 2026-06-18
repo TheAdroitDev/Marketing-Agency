8cb27ab7651ac4a69bc328f8bafa2c66a90890dd
GitPirates <starshivamverma2@gmail.com>
2026-06-18 16:20:28 +0530

    feat: add CoverDemo component to showcase cover functionality

---
+++ components/cover-demo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Cover } from "@/components/ui/cover";
+
+export default function CoverDemo() {
+  return (
+    <div>
+      <h1 >
+      <Cover>Real Results</Cover>
+      </h1>
+    </div>
+  );
+}
